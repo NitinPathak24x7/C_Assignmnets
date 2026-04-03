b1ef9a79ffd144a7a59be083fc8d3803ed221494
NitinPathak <nitinpathak24x7@gmail.com>
2026-04-03 14:06:33 +0530

    first assignment

---
+++ Assignment 1/q3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main (){
+
+/* 
+printf("'");
+printf("'");
+printf("MySirG");
+printf("'");
+printf("");
+ */
+
+ /* char c='"';
+ printf("%c",c);
+ printf("MySirG");
+ printf("%c",c);
+ */
+ 
+     printf("\"MySirG\"");
+    
+    return 0;
+}   
